1a73446924bb91378d55926c356049e6c48fca70
meenachisundaram-B <zhadigitalclass@gmail.com>
2025-05-16 15:59:56 +0530

    Delete src/app directory

--- src/app/components/brandinglink.jsx
+++
@@ -1,8 +0,0 @@
-import Link from 'next/link'
-
-<Link
-  href="/services/branding"
-  className="inline-block mt-6 text-white underline hover:text-teal-300 transition"
->
-  Explore Branding Services →
-</Link>
--- src/app/components/videolink.jsx
+++
@@ -1,8 +0,0 @@
-import Link from 'next/link'
-
-<Link
-  href="/services/video"
-  className="inline-block mt-6 text-white underline hover:text-teal-300 transition"
->
-  Explore Video Services →
-</Link>
